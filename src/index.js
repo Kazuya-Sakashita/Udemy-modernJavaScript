c5a6d2d3445f92c9f9894865b57e8e908c96868a
Kazuya-Sakashita <64903209+Kazuya-Sakashita@users.noreply.github.com>
2022-04-17 15:57:16 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+/**
+ * const.let等の変数宣言
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変ん数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// val1 = "Var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは際宣言不可能
+// let val2 = "let変数変数際宣言";
+
+// const val3 = "const変数宣言";
+// console.log(val3);
+
+// //const変数は上書き不可
+// // val3 = "const変数上書き"
+
+// const val3 = "const変数を際宣言";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+const val4 = {
+  name: "かずや",
+  age: 50
+};
+val4.name = "KAZUYA";
+val4.addres = "osaka";
+console.log(val4);
