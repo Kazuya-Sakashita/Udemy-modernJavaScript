ae05f359f03d8652fe6110d370320289e9959acb
Kazuya-Sakashita <kz0508@gmail.com>
2022-04-17 20:16:16 +0900

    セクション１９終了

--- src/index.js
+++ src/index.js
@@ -46,3 +46,21 @@
 // val5[0] = "bird";
 // val5.push("monky")
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+
+const name = "かずや";
+const age = 50;
+
+//「私の名前はかずやです。年齢は50歳です。」
+
+// 従来方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
