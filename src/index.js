596f27329f12f634bf6e59fa0f87b7c5f2da4dba
Kazuya-Sakashita <kz0508@gmail.com>
2022-04-17 20:27:00 +0900

    セクション２０終了

--- src/index.js
+++ src/index.js
@@ -52,15 +52,40 @@
  */
 
 
-const name = "かずや";
-const age = 50;
+// const name = "かずや";
+// const age = 50;
 
-//「私の名前はかずやです。年齢は50歳です。」
+// //「私の名前はかずやです。年齢は50歳です。」
 
-// 従来方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-// console.log(message1);
+// // 従来方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// // console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+// 従来の関数
+
+// function func1(str) {
+//     return str;
+// }
+const func1 = function(str) {
+ return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1,num2) => {
+    return num1 + num2;
+};
+console.log(func3(10,20))
