26b9b5b0150c5cce1bc48307fcbd5ded1041d6e2
Kazuya-Sakashita <kz0508@gmail.com>
2022-04-17 16:32:35 +0900

    セクション１８終了

--- src/index.js
+++ src/index.js
@@ -39,3 +39,10 @@
 // val4.name = "KAZUYA";
 // val4.addres = "osaka";
 // console.log(val4);
+
+
+//constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monky")
+// console.log(val5);
