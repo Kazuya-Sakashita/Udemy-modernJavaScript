7113a7088e9a8ffb11efe74e1d7eadf44aad17ff
Kazuya-Sakashita <kz0508@gmail.com>
2022-04-21 21:01:45 +0900

    セクション２５　三項演算子終了

--- src/index.js
+++ src/index.js
@@ -166,7 +166,7 @@
  * mapやfilterを使った配列の処理
  */
 
-const nameArr = ["田中", "山田","坂下"];
+// const nameArr = ["田中", "山田","坂下"];
 // for (let index = 0; index < nameArr.length; index++) {
 //     console.log(nameArr[index]);
 // }
@@ -185,11 +185,33 @@ const nameArr = ["田中", "山田","坂下"];
 // });
 // console.log(newArr);
 
-const newNameArr = nameArr.map((name)=>{
-    if (name === '坂下'){
-        return name
-    } else {
-        return `${name}さん`
-    }
-})
-console.log(newNameArr)
+// const newNameArr = nameArr.map((name)=>{
+//     if (name === '坂下'){
+//         return name
+//     } else {
+//         return `${name}さん`
+//     }
+// })
+// console.log(newNameArr)
+
+/**
+ * 三項演算子
+ */
+
+//使いすぎると可読性が悪くなるので使うところに注意
+// ある条件？　条件がtureの時　:条件がfalseの時
+
+// const val1 = 1 > 0 ? 'tureです' : 'falseです';
+// console.log(val1);
+
+
+// const num = 2500;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number'? num.toLocaleString() :'数値を入力してください'
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => { 
+//     return num1 + num2 > 100 ? '１００を超えています！！' : '許容範囲内です';
+// }
+// console.log(checkSum(50,40));
