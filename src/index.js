8beb2e22edac7fe0b550caa03559153a7f882619
Kazuya-Sakashita <kz0508@gmail.com>
2022-04-21 20:47:08 +0900

    セクション２４終了

--- src/index.js
+++ src/index.js
@@ -116,8 +116,80 @@
 // console.log(massage4);
 
 /**
- * デフォルト血、引数など
+ * デフォルト値、引数など
  */
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
-sayHello( "KAZUYA");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello( "KAZUYA");
+
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+
+// const arr1 = [1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、統合
+//  const arr4 = [10, 20];
+//  const arr5 = [30, 40];
+
+//  const arr6 =[...arr4];
+//  arr6[0] = 100; 
+// console.log(arr6);
+// console.log(arr4);
+
+//  const arr7 =[...arr4, ...arr5];
+//  console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;    //arr4までデータが置き換わる
+// console.log(arr8); 
+// console.log(arr4); 
+
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+const nameArr = ["田中", "山田","坂下"];
+// for (let index = 0; index < nameArr.length; index++) {
+//     console.log(nameArr[index]);
+// }
+
+
+// const nameArr2 = nameArr.map((name)=>{
+//     return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name)=> console.log(name));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newArr = numArr.filter((num) =>{
+//     return num % 2 === 0;
+// });
+// console.log(newArr);
+
+const newNameArr = nameArr.map((name)=>{
+    if (name === '坂下'){
+        return name
+    } else {
+        return `${name}さん`
+    }
+})
+console.log(newNameArr)
